2d7af0960f19b1d132d50aae7787d9fbe1fe7909
unokun <unokun@gmail.com>
2021-04-09 14:57:23 +0900

    Section 2 Create the Row View

--- Landmarks/Model/Landmark.swift
+++ Landmarks/Model/Landmark.swift
@@ -4,28 +4,30 @@
 //
 //  Created by Masaaki Uno on 2021/04/09.
 //
-
 import Foundation
 import SwiftUI
 import CoreLocation
 
-struct Landmark: Hashable, Codable {
+struct Landmark: Hashable, Codable, Identifiable {
     var id: Int
     var name: String
     var park: String
     var state: String
     var description: String
-    
-    private var imageNmae: String
+
+    private var imageName: String
     var image: Image {
-        Image(imageNmae)
+        Image(imageName)
     }
-    
-    private var coordinates: Coodinates
+
+    private var coordinates: Coordinates
     var locationCoordinate: CLLocationCoordinate2D {
-        CLLocationCoordinate2D(latitude: coordinates.latitude, longitude: coordinates.longitude)
+        CLLocationCoordinate2D(
+            latitude: coordinates.latitude,
+            longitude: coordinates.longitude)
     }
-    struct Coodinates: Hashable, Codable {
+
+    struct Coordinates: Hashable, Codable {
         var latitude: Double
         var longitude: Double
     }
