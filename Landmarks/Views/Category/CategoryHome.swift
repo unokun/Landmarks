f1139955694cf9051237b3a1be8032fffc9cb7bc
unokun <unokun@gmail.com>
2021-04-11 13:32:43 +0900

    Composing Complex Interfaces Section 1 Add a Category View

---
+++ Landmarks/Views/Category/CategoryHome.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Masaaki Uno on 2021/04/11.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+    var body: some View {
+        NavigationView {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                .navigationTitle("Featured")
+        }
+    }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
