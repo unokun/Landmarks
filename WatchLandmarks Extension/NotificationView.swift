fedbd78e63480b12db3d67de95491da68789962f
unokun <unokun@gmail.com>
2021-04-12 15:22:09 +0900

    Section 5 Create a Custom Notification Interface

--- WatchLandmarks Extension/NotificationView.swift
+++ WatchLandmarks Extension/NotificationView.swift
@@ -8,13 +8,30 @@
 import SwiftUI
 
 struct NotificationView: View {
+    var title: String?
+    var message: String?
+    var landmark: Landmark?
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            if landmark != nil {
+                CircleImage(image: landmark!.image.resizable())
+                    .scaledToFit()
+            }
+            
+            Text(title ?? "Unknown landmark")
+                .font(.headline)
+            
+            Divider()
+            
+            Text(message ?? "You are within 5 miles of one of your favorite landmarks.")
+        }
     }
 }
 
 struct NotificationView_Previews: PreviewProvider {
     static var previews: some View {
         NotificationView()
+        NotificationView(title: "Tuttle Rock", message: "You are within 5 miles of Turtle Rock.", landmark: ModelData().landmarks[0])
     }
 }
