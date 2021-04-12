23191021011ebff4daa66de9df1cf16b75866c38
unokun <unokun@gmail.com>
2021-04-12 15:35:41 +0900

    Creating a macOS App Section 1 Add a macOS Target to the Project

---
+++ MacLandmarks/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  MacLandmarks
+//
+//  Created by Masaaki Uno on 2021/04/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+            .frame(minWidth: 700, minHeight: 300)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .environmentObject(ModelData())
+    }
+}
