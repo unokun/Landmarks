aff55cc91d266d2a8b13548bf6696fd9ca1f1a20
unokun <unokun@gmail.com>
2021-04-14 10:35:39 +0900

    Section 7 Add Preferences with a Settings Scene

--- Landmarks/LandmarksApp.swift
+++ Landmarks/LandmarksApp.swift
@@ -28,5 +28,11 @@ struct LandmarksApp: App {
         #if os(watchOS)
         WKNotificationScene(controller: NotificationController.self, category: "LandmarkNear")
         #endif
+        
+        #if os(macOS)
+        Settings {
+            LandmarkSettings()
+        }
+        #endif
     }
 }
